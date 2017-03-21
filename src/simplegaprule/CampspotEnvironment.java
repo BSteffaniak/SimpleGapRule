52e187abcbcb3dbe3e8ea4c9dcdb9f09663de4d0
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 16:07:32 -0700

    Added CampspotEnvironment bean

---
+++ src/simplegaprule/CampspotEnvironment.java
@@ -0,0 +1,45 @@
+package simplegaprule;
+
+public class CampspotEnvironment {
+	private Search search;
+	
+	private GapRule[] gapRules;
+	private Campsite[] campsites;
+	private Reservation[] reservations;
+	
+	public CampspotEnvironment() {
+		
+	}
+	
+	public Search getSearch() {
+		return search;
+	}
+	
+	public void setSearch(Search search) {
+		this.search = search;
+	}
+	
+	public GapRule[] getGapRules() {
+		return gapRules;
+	}
+	
+	public void setGapRules(GapRule[] gapRules) {
+		this.gapRules = gapRules;
+	}
+	
+	public Campsite[] getCampsites() {
+		return campsites;
+	}
+	
+	public void setCampsites(Campsite[] campsites) {
+		this.campsites = campsites;
+	}
+	
+	public Reservation[] getReservations() {
+		return reservations;
+	}
+	
+	public void setReservations(Reservation[] reservations) {
+		this.reservations = reservations;
+	}
+}
