8ea4a0290244b07373e8b20c7fd6fe5d421fbbde
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 15:48:20 -0700

    Added Search bean

---
+++ src/simplegaprule/Search.java
@@ -0,0 +1,27 @@
+package simplegaprule;
+
+import java.util.Date;
+
+public class Search {
+	private Date startDate, endDate;
+	
+	public Search() {
+		
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+}
