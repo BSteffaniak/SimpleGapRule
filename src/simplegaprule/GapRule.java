43b9483b7cbade2d3e837c085a0b9c37bed28aed
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 15:55:13 -0700

    Added GapRule bean

---
+++ src/simplegaprule/GapRule.java
@@ -0,0 +1,17 @@
+package simplegaprule;
+
+public class GapRule {
+	private int gapSize;
+	
+	public GapRule() {
+		
+	}
+	
+	public int getGapSize() {
+		return gapSize;
+	}
+	
+	public void setGapSize(int gapSize) {
+		this.gapSize = gapSize;
+	}
+}
