b8a8c0d890c3d95bec8b5f7b2121ddd179a25a29
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 19:00:08 -0700

    Added reservation content test

--- src/tests/simplegaprule/GapRuleModelTests.java
+++ src/tests/simplegaprule/GapRuleModelTests.java
@@ -49,4 +49,38 @@ public class GapRuleModelTests
 			assertEquals(values[i], site.getName());
 		}
 	}
+	
+	@Test
+	public void testReservationContent() {
+		int[] ids = new int[] { 1, 1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9 };
+		String[] dateRanges = new String[] {
+			"2016-06-01 to 2016-06-04",
+			"2016-06-11 to 2016-06-13",
+			"2016-06-08 to 2016-06-09",
+			"2016-06-04 to 2016-06-06",
+			"2016-06-14 to 2016-06-16",
+			"2016-06-03 to 2016-06-05",
+			"2016-06-13 to 2016-06-14",
+			"2016-06-03 to 2016-06-06",
+			"2016-06-12 to 2016-06-14",
+			"2016-06-04 to 2016-06-06",
+			"2016-06-11 to 2016-06-12",
+			"2016-06-16 to 2016-06-16",
+			"2016-06-03 to 2016-06-04",
+			"2016-06-07 to 2016-06-09",
+			"2016-06-13 to 2016-06-16",
+			"2016-06-01 to 2016-06-02",
+			"2016-06-05 to 2016-06-06",
+			"2016-06-03 to 2016-06-05",
+			"2016-06-12 to 2016-06-16"
+		};
+		
+		for (int i = 0; i < ids.length; i++)
+		{
+			Reservation reservation = rule.getEnvironment().getReservations()[i];
+			
+			assertEquals(ids[i], reservation.getCampsiteId());
+			assertEquals(dateRanges[i], reservation.getStartDate().toString(SimpleGapRule.DEFAULT_DATE_FORMAT) + " to " + reservation.getEndDate().toString(SimpleGapRule.DEFAULT_DATE_FORMAT));
+		}
+	}
 }
