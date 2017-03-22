e289be6a814533f81b4b71c18ed463acbdbbd1d9
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 21:51:39 -0700

    Added other gap rule tests

--- src/tests/simplegaprule/GapRuleTests.java
+++ src/tests/simplegaprule/GapRuleTests.java
@@ -1,14 +1,12 @@
 package simplegaprule;
 
-import org.junit.Before;
 import org.junit.Test;
 import simplegaprule.models.Campsite;
-import simplegaprule.models.GapRule;
-import simplegaprule.models.Reservation;
 
 import java.io.File;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class GapRuleTests
 {
@@ -16,6 +14,41 @@ public class GapRuleTests
 	public void testGivenTestCase() {
 		SimpleGapRuleProgram rule = new SimpleGapRuleProgram(new File("test-case.json"));
 		
-		rule.getAvailableCampsites();
+		List<Campsite> available = rule.getAvailableCampsites();
+		
+		assertEquals(4, available.size());
+		assertEquals("Daniel Boone Bungalow", available.get(0).getName());
+		assertEquals("Teddy Rosevelt Tent Site", available.get(1).getName());
+		assertEquals("Bear Grylls Cozy Cave", available.get(2).getName());
+		assertEquals("Wyatt Earp Corral", available.get(3).getName());
+	}
+	
+	@Test
+	public void testNoOpenings() {
+		SimpleGapRuleProgram rule = new SimpleGapRuleProgram(new File("test-folder/no-openings.json"));
+		
+		List<Campsite> available = rule.getAvailableCampsites();
+		
+		assertEquals(0, available.size());
+	}
+	
+	@Test
+	public void testNoReservations() {
+		SimpleGapRuleProgram rule = new SimpleGapRuleProgram(new File("test-folder/no-reservations.json"));
+		
+		List<Campsite> available = rule.getAvailableCampsites();
+		
+		assertEquals(rule.getEnvironment().getCampsites().length, available.size());
+		assertEquals(0, rule.getEnvironment().getReservations().length);
+	}
+	
+	@Test
+	public void testSingleReservationConflict() {
+		SimpleGapRuleProgram rule = new SimpleGapRuleProgram(new File("test-folder/one-reservation.json"));
+		
+		List<Campsite> available = rule.getAvailableCampsites();
+		
+		assertEquals(rule.getEnvironment().getCampsites().length - 1, available.size());
+		assertEquals(1, rule.getEnvironment().getReservations().length);
 	}
 }
