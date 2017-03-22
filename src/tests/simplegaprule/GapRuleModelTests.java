743dcd993e15cce4e1b532034f0c96681a1c576a
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 18:43:55 -0700

    Added testCampsiteContent function

--- src/tests/simplegaprule/GapRuleModelTests.java
+++ src/tests/simplegaprule/GapRuleModelTests.java
@@ -26,4 +26,27 @@ public class GapRuleModelTests
 	public void testCampsiteCount() {
 		assertEquals(9, rule.getEnvironment().getCampsites().length);
 	}
+	
+	@Test
+	public void testCampsiteContent() {
+		String[] values = new String[] {
+			"Grizzly Adams Adventure Cabin",
+			"Lewis and Clark Camp Spot",
+			"Jonny Appleseed Log Cabin",
+			"Davey Crockett Camphouse",
+			"Daniel Boone Bungalow",
+			"Teddy Rosevelt Tent Site",
+			"Edmund Hillary Igloo",
+			"Bear Grylls Cozy Cave",
+			"Wyatt Earp Corral"
+		};
+		
+		for (int i = 0; i < values.length; i++)
+		{
+			Campsite site = rule.getEnvironment().getCampsites()[i];
+			
+			assertEquals(i + 1, site.getId());
+			assertEquals(values[i], site.getName());
+		}
+	}
 }
