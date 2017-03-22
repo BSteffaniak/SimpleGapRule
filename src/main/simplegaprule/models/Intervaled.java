13011ddc1c4430fa9b8a9f82c369916addb5ae0d
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-21 21:49:21 -0700

    Added documentation

--- src/main/simplegaprule/models/Intervaled.java
+++ src/main/simplegaprule/models/Intervaled.java
@@ -11,22 +11,51 @@ public interface Intervaled {
 	void setStartDate(DateTime start);
 	void setEndDate(DateTime start);
 	
+	/**
+	 * Get the Joda Interval for this Intervaled instance.
+	 * 
+	 * @return The Joda Interval instance.
+	 */
 	default Interval getInterval() {
 		return new Interval(getStartDate(), getEndDate());
 	}
 	
+	/**
+	 * Check if this interval is before the given interval.
+	 *
+	 * @param other The interval to compare to.
+	 * @return true if this interval is before other.
+	 */
 	default boolean isBefore(Intervaled other) {
 		return getInterval().isBefore(other.getInterval());
 	}
 	
+	/**
+	 * Check if this interval is after the given interval.
+	 * 
+	 * @param other The interval to compare to.
+	 * @return true if this interval is after other.
+	 */
 	default boolean isAfter(Intervaled other) {
 		return getInterval().isAfter(other.getInterval());
 	}
 	
+	/**
+	 * Check if this interval overlaps the other interval.
+	 * 
+	 * @param other The other interval to check if overlaps.
+	 * @return true if this interval overlaps the other.
+	 */
 	default boolean doesOverlap(Intervaled other) {
 		return getInterval().overlaps(other.getInterval());
 	}
 	
+	/**
+	 * Get the days in the gap between the two intervals.
+	 * 
+	 * @param other The interval to get the gap between.
+	 * @return An int representation of the days gap.
+	 */
 	default int getGap(Intervaled other) {
 		return getInterval().gap(other.getInterval()).toPeriod().getDays();
 	}
